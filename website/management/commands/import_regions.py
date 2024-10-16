b446e859b00c0caeb81ca00012fe5bf79432fe50
amneet uppal <amneetuppal@icloud.com>
2024-10-16 13:14:39 +0100

    Fixed data import logic to resolve issues with regions 'East' and 'Yorkshire and the Humber'

--- website/management/commands/import_regions.py
+++ website/management/commands/import_regions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from django.core.management.base import BaseCommand
 from website.models import Region
+from django.db import IntegrityError
 
 class Command(BaseCommand):
     help = 'Import data from CSV'
@@ -9,16 +10,24 @@ class Command(BaseCommand):
         file_path = 'data/debtstats.csv'
         
         df = pd.read_csv(file_path)
-
+  
         for _, row in df.iterrows():
-            Region.objects.create(
-                region_name=row['region_name'],
-                percentage=row['percentage'],
-                median=row['median'],
-                percentile_25=row['percentile_25'],
-                percentile_75=row['percentile_75'],
-                frequency=row['frequency'],
-                frequency_legend=row['frequency_legend'],
-            )
+            region_name = row['region_name']
+            if not Region.objects.filter(region_name=region_name).exists():
+                try:
+                    Region.objects.create(
+                        region_name=region_name,
+                        percentage=row['percentage'],
+                        median=row['median'],
+                        percentile_25=row['percentile_25'],
+                        percentile_75=row['percentile_75'],
+                        frequency=row['frequency'],
+                        frequency_legend=row['frequency_legend'],
+                    )
+                    self.stdout.write(self.style.SUCCESS(f'Successfully added region: {region_name}'))
+                except IntegrityError:
+                    self.stdout.write(self.style.WARNING(f'Region {region_name} already exists, skipping...'))
+            else:
+                self.stdout.write(self.style.WARNING(f'Region {region_name} already exists, skipping...'))
 
-        self.stdout.write(self.style.SUCCESS('Data import from CSV successful.'))
+        self.stdout.write(self.style.SUCCESS('Data import from CSV completed.'))
--- website/models.py
+++ website/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Region(models.Model):
-    region_name = models.CharField(max_length=50)
+    region_name = models.CharField(max_length=50, unique=True)
     percentage = models.IntegerField()
     median = models.IntegerField()
     percentile_25 = models.IntegerField()
