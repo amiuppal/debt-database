5f2b95f24d092373c64c70f2c41d475a8f17fade
Harsh Bajwa <90355783+harshsbajwa@users.noreply.github.com>
2024-10-16 03:11:38 -0400

    API fix to return precentile_75 and frequency

--- website/management/commands/import_regions.py
+++ website/management/commands/import_regions.py
@@ -1,16 +1,18 @@
 import pandas as pd
 from django.core.management.base import BaseCommand
-from website.models import Region  
+from website.models import Region
 
 class Command(BaseCommand):
-    help = 'Import data from Excel'
+    help = 'Import data from CSV'
 
     def handle(self, *args, **kwargs):
-        df = pd.read_excel('data/debtstats.xls')
+        file_path = 'data/debtstats.csv'
+        
+        df = pd.read_csv(file_path)
 
         for _, row in df.iterrows():
             Region.objects.create(
-                region_name=row['region_name'],  
+                region_name=row['region_name'],
                 percentage=row['percentage'],
                 median=row['median'],
                 percentile_25=row['percentile_25'],
@@ -19,4 +21,4 @@ class Command(BaseCommand):
                 frequency_legend=row['frequency_legend'],
             )
 
-        self.stdout.write(self.style.SUCCESS('Data import successful.'))
+        self.stdout.write(self.style.SUCCESS('Data import from CSV successful.'))
--- website/models.py
+++ website/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Region(models.Model):
     region_name = models.CharField(max_length=30)
-    percentage = models.DecimalField(max_digits=10, decimal_places=2)
-    median = models.DecimalField(max_digits=10, decimal_places=2)
-    percentile_25 = models.DecimalField(max_digits=10, decimal_places=2)
-    percentile_75 = models.CharField(max_length=10)
-    frequency = models.CharField(max_length=30)
+    percentage = models.IntegerField()
+    median = models.IntegerField()
+    percentile_25 = models.IntegerField()
+    percentile_75 = models.IntegerField()
+    frequency = models.IntegerField()
     frequency_legend = models.CharField(max_length=30)
 
     def __str__(self):
